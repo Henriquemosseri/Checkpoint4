c1f4fe22ff0b3272a56e3d5afb7e857895a34826
Henrique Mosseri <henriquemosseri@gmail.com>
2025-09-10 20:40:35 -0300

    Adicionar arquivos de projeto.

---
+++ Model/Users.cs
@@ -0,0 +1,11 @@
+﻿namespace performance_cache.Model
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string  Email { get; set; }
+        public DateTime UltimoAcesso { get; set; } 
+
+    }
+}
